31fa0328a03bd874b996715c0f1ac4cdc6557899
devkyr103 <devkyr103@gmail.com>
2023-06-20 03:31:36 +0900

    Index Buffer, Constant Buffer 추가

--- kyrEngine/Engine_SOURCE/kyrGraphicDevice_Dx11.h
+++ kyrEngine/Engine_SOURCE/kyrGraphicDevice_Dx11.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "kyrEngine.h"
+#include "kyrGraphics.h"
 
 #include <d3d11.h>
 #include <d3dcompiler.h>
@@ -24,6 +25,10 @@ namespace kyr::graphics
 		bool CreateTexture(const D3D11_TEXTURE2D_DESC* desc, void* data);
 
 		void BindViewPort(D3D11_VIEWPORT* viewPort);
+
+		void SetConstantBuffer(ID3D11Buffer* buffer, void* data, UINT size);
+		void BindConstantBuffer(eShaderStage stage, eCBType type, ID3D11Buffer* buffer);
+		void BindsConstantBuffer(eShaderStage stage, eCBType type, ID3D11Buffer* buffer);
 
 		void Draw();
 
--- kyrEngine/Engine_SOURCE/kyrRenderer.cpp
+++ kyrEngine/Engine_SOURCE/kyrRenderer.cpp
@@ -10,6 +10,10 @@ namespace kyr::renderer
 	// Vertex Buffer
 	ID3D11Buffer* triangleBuffer = nullptr;
 
+	// Index Buffer
+	ID3D11Buffer* triangleIdxBuffer = nullptr;
+	ID3D11Buffer* triangleConstantBuffer = nullptr;
+
 	// error blob
 	ID3DBlob* errorBlob = nullptr;
 
@@ -32,6 +36,7 @@ namespace kyr::renderer
 
 	void LoadBuffer()
 	{
+		//Vertex Buffer
 		D3D11_BUFFER_DESC triangleDesc = {};
 		triangleDesc.Usage = D3D11_USAGE::D3D11_USAGE_DYNAMIC;
 		triangleDesc.ByteWidth = sizeof(Vertex) * 3;
@@ -41,6 +46,37 @@ namespace kyr::renderer
 		D3D11_SUBRESOURCE_DATA triangleData = {};
 		triangleData.pSysMem = vertexes;
 		kyr::graphics::GetDevice()->CreateBuffer(&triangleBuffer, &triangleDesc, &triangleData);
+
+		
+		std::vector<UINT> indexes = {};
+		indexes.push_back(0);
+		indexes.push_back(1);
+		indexes.push_back(2);
+
+
+		// Index Buffer
+		D3D11_BUFFER_DESC triangleIdxDesc = {};
+		triangleIdxDesc.ByteWidth = sizeof(UINT) * indexes.size();
+		triangleIdxDesc.BindFlags = D3D11_BIND_FLAG::D3D11_BIND_INDEX_BUFFER;
+		triangleIdxDesc.Usage = D3D11_USAGE_DEFAULT;
+		triangleIdxDesc.CPUAccessFlags = 0;
+
+		D3D11_SUBRESOURCE_DATA triangleIdxData = {};
+		triangleIdxData.pSysMem = indexes.data();
+		kyr::graphics::GetDevice()->CreateBuffer(&triangleIdxBuffer, &triangleIdxDesc, &triangleIdxData);
+
+		// Constant Buffer
+		D3D11_BUFFER_DESC triangleCSDesc = {};
+		triangleCSDesc.ByteWidth = sizeof(Vector4);
+		triangleCSDesc.BindFlags = D3D11_BIND_FLAG::D3D11_BIND_CONSTANT_BUFFER;
+		triangleCSDesc.Usage = D3D11_USAGE::D3D11_USAGE_DYNAMIC;
+		triangleCSDesc.CPUAccessFlags = D3D11_CPU_ACCESS_WRITE;
+
+		kyr::graphics::GetDevice()->CreateBuffer(&triangleConstantBuffer, &triangleCSDesc, nullptr);
+
+		Vector4 pos(0.3f, 0.0f, 0.0f, 1.0f);
+		kyr::graphics::GetDevice()->SetConstantBuffer(triangleConstantBuffer, &pos, sizeof(Vector4));
+		kyr::graphics::GetDevice()->BindConstantBuffer(eShaderStage::VS, eCBType::Transform, triangleConstantBuffer);
 	}
 
 	void LoadShader()
