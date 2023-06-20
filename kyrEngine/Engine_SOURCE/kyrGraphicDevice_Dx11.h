4f62610d83f157f3247fb8aec6962c0719f7195c
devkyr103 <devkyr103@gmail.com>
2023-06-20 12:42:14 +0900

    Texture Class 추가 - 이미지 로드 및 ShaderReousrceView 생성 - BIndShader

--- kyrEngine/Engine_SOURCE/kyrGraphicDevice_Dx11.h
+++ kyrEngine/Engine_SOURCE/kyrGraphicDevice_Dx11.h
@@ -32,7 +32,8 @@ namespace kyr::graphics
 		void SetConstantBuffer(ID3D11Buffer* buffer, void* data, UINT size);
 		void BindConstantBuffer(eShaderStage stage, eCBType type, ID3D11Buffer* buffer);
 		void BindsConstantBuffer(eShaderStage stage, eCBType type, ID3D11Buffer* buffer);
-		
+		void BindShaderResource(eShaderStage stage, UINT startSlot, ID3D11ShaderResourceView** ppSRV);
+
 		void BindViewPort(D3D11_VIEWPORT* viewPort);
 
 		void DrawIndexed(UINT IndexCount, UINT StartIndexLocation, INT BaseVertexLocation);
@@ -40,6 +41,8 @@ namespace kyr::graphics
 		void UpdateViewPort();
 		void Draw();
 		void Present();
+
+		ID3D11Device* GetID3D11Device() { return mDevice.Get(); }
 
 	private:
 		// 실제 그래픽카드 하드웨어 객체
